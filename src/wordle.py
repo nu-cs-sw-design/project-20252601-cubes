193490dc834f543508417f12a82d903930c728ad
Kim Danny <kimdanny@dhcp-10-105-231-72.wireless.northwestern.private>
2025-11-20 18:07:38 -0600

    Added get_random_word method

--- src/wordle.py
+++ src/wordle.py
@@ -1,5 +1,12 @@
 from enum import Enum 
 from collections import Counter
+import random
+import os
+
+class Color(Enum):
+    GREEN = "🟩"
+    YELLOW = "🟨"
+    GRAY = "⬜"
 
 def evaluate_guess(guess:str, target:str):
     word_length = len(target)
@@ -11,20 +18,40 @@ def evaluate_guess(guess:str, target:str):
 
     for i in range(word_length):
         if guess[i] == target[i]:
-            result[i] = "GREEN"
+            result[i] = Color.GREEN.value
             target_letter_count[guess[i]] -= 1
     
     for i in range(word_length):
-        if result[i] == "GREEN":
+        if result[i] == Color.GREEN.value:
             continue
 
         if target_letter_count[guess[i]] > 0:
-            result[i] == "YELLOW"
+            result[i] == Color.YELLOW.value
             target_letter_count[guess[i]] -= 1
         else:
-            result[i] = 'GRAY'
+            result[i] = Color.GRAY.value
+
 
     return result
 
-a = evaluate_guess("HELLO", "FROWN")
-print(a)
+def format_guess_result(result):
+    a = ""
+    for i in result:
+        a += i
+    
+    return a
+
+def get_random_word(file):
+    dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(dir, file)
+    with open(file_path, 'r') as f:
+        words = set(f.read().splitlines())
+    return random.sample(words, 1)[0].upper()
+
+
+word = get_random_word("words.txt")
+print(word)
+
+#a = evaluate_guess("HLOWN", "FROWN")
+#print(format_guess_result(a))
+
