d8d4ef4940a8f7c03570e3fabb315fda8bee3e8f
Kim Danny <kimdanny@Mac.lan>
2025-11-26 12:31:41 -0500

    Restructed prototype

---
+++ code/index.js
@@ -0,0 +1,11 @@
+class Wordle {
+  constructor(wordLength) {
+    this.wordLength = wordLength;
+    this.maxAttempts = 6;
+    this.gridContainer = document.getElementById("grid-container");
+  }
+
+  createGrid() {
+    this.g;
+  }
+}
