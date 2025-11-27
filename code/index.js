393b7e9aff6f0ee61c6af3df882a2404eae688ac
Kim Danny <kimdanny@Kims-MacBook-Pro.local>
2025-11-27 11:28:03 -0500

    Reformated CSS

--- code/index.js
+++ code/index.js
@@ -3,9 +3,23 @@ class Wordle {
     this.wordLength = wordLength;
     this.maxAttempts = 6;
     this.gridContainer = document.getElementById("grid-container");
+    this.createGrid();
   }
 
   createGrid() {
-    this.g;
+    for (let i = 0; i < this.maxAttempts; i++) {
+      const row = document.createElement("div");
+      row.classList.add("grid-row");
+      for (let j = 0; j < this.wordLength; j++) {
+        const tile = document.createElement("div");
+        tile.classList.add("grid-tile");
+        tile.dataset.row = i;
+        tile.dataset.col = j;
+        row.appendChild(tile);
+      }
+      this.gridContainer.appendChild(row);
+    }
   }
 }
+
+const wordleGame = new Wordle(6);
